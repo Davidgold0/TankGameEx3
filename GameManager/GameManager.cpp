7b002f4f27ac6b78df1f99238a48d4243408e2db
Davidgold0 <david4@mail.tau.ac.il>
2025-08-25 00:17:11 +0300

    s

--- GameManager/GameManager.cpp
+++ GameManager/GameManager.cpp
@@ -9,8 +9,9 @@ using namespace std;
 using namespace UserCommon_208000547_208000547;
 namespace GameManager_208000547_208000547 {
 
-GameManager::GameManager(bool verbose) : verbose_(verbose),
+GameManager::GameManager(bool verbose) : 
     creationOrderCounter(0),
+    verbose_(verbose),
     allTanksOutOfShells(false),
     roundsSinceNoShells(0)
 {}
@@ -921,14 +922,16 @@ void GameManager::runGameLoop() {
         gameData.player2TankCount);
 }
 
-GameResult GameManager::run(
-    size_t map_width, size_t map_height,
+GameResult GameManager::run(size_t map_width, size_t map_height,
     const SatelliteView& map, // <= a snapshot, NOT updated
     string map_name,
     size_t max_steps, size_t num_shells,
     Player& player1, string name1, Player& player2, string name2, 
     TankAlgorithmFactory player1_tank_algo_factory,
     TankAlgorithmFactory player2_tank_algo_factory) {
+    
+    (void)name1; // Suppress unused parameter warning
+    (void)name2; // Suppress unused parameter warning
     
     std::cout << "\nStarting game with new interface..." << std::endl;
     
